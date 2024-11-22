50bc8abaf0b582eff4d2ebcb0a0a495502f0f2c2
solitude <fitpex.m@gmail.com>
2024-11-22 21:56:51 +0300

    Add Chat/Messages classes

--- main.py
+++ main.py
@@ -14,3 +14,43 @@ class User:
         self.status = status
 
 
+class Message:
+    def __init__(self, message_id, sender, recipient, content, timestamp, is_read=False):
+        self.message_id = message_id
+        self.sender = sender
+        self.recipient = recipient
+        self.content = content
+        self.timestamp = timestamp
+        self.is_read = is_read
+
+    def mark_as_read(self):
+        self.is_read = True
+
+
+class Chat:
+    def __init__(self, chat_id, participants):
+        self.chat_id = chat_id
+        self.participants = participants
+        self.messages = []
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def get_last_message(self):
+        return self.messages[-1] if self.messages else None
+
+
+class GroupChat(Chat):
+    def __init__(self, chat_id, participants, group_name):
+        super().__init__(chat_id, participants)
+        self.group_name = group_name
+
+    def add_participant(self, user):
+        if user not in self.participants:
+            self.participants.append(user)
+
+    def remove_participant(self, user):
+        if user in self.participants:
+            self.participants.remove(user)
+
+
